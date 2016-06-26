8790e4d7d680d2b1ed5416ce8d378e524cb9de07
andywang02 <andywang02@gmail.com>
2016-06-25 22:20:45 -0700

    New drive.h

--- createDrive.h
+++ createDrive.h
@@ -1,47 +1,101 @@
-//#define createRightOffset 1 //when lower, create goes left
-//#define createLeftOffset 0.725 //when lower, create goes right
-#define createLeftOffset 1 //when lower, create goes left
-#define createRightOffset 0.98669 //when lower, create goes right   0.9867 - old
-#define createDriveOffset 1
-#define armMotorF 0
-#define armMotorB 1
-#define FArmOffset 1
-#define BArmOffset 0.88
-#define createTurnOffset 0.987
-//#define createTurnOffset 1
-#define FClawServo 1
-#define FClawClose 0
-#define FClawOpen 1331
-#define BClawServo 0
-#define BClawClose 1952
-#define BClawOpen 59
-#define clawPickUp 86
-#define touchPortF 0
-#define touchPortB 1
-#define grabPort 2
-#define up_pos 100
-#define release_pos 274
-#define grab_pos 1025
+/**************************************
+			Port Definitons
+**************************************/
 
-void createConnectAll();
-void createDrive(float speed, float distance);
-void createDriveBack(float speed, float distance);
-void createDriveUntilTouch(float speed);
-void driveUntil(float speed, float time);
-void createSquareUp(float speed, float time);
-void createTurnRight(float speed, int degrees);
-void createTurnLeft(float speed, int degrees);
-void createArmUp(float speed);
-void createArmDown(float speed);
-void createArmUpOffset(float speed, float FOffset, float BOffset);
-void createArmDownOffset(float speed, float FOffset, float BOffset);
-void createArmUpOffsetTime(float speed, float time, float FOffset, float BOffset);
-void createArmDownOffsetTime(float speed, float time, float FOffset, float BOffset);
-void createArmStay(float speed);
-void createFClawOpen();
-void createFClawClose();
-void createBClawOpen();
-void createBClawClose();
-void pipeUp();
-void grabPipe();
-void releasePipe();
+#define clawMoverPort 0
+#define clawPort 1
+#define miniMoverPort 2
+#define binPort 3
+#define touchPort 0
+
+/**************************************
+			Claw Definitons
+**************************************/            
+              
+#define clawOpenPos 1600
+#define clawClosePos 900
+#define clawDownPos 400
+#define clawCarryPos 775
+#define clawUpPos 2030
+#define clawSquareUpPos 1100
+
+/**************************************
+		  Mini Claw Definitons
+**************************************/              
+              
+#define miniDownPos 2000
+#define miniUpPos 0
+#define miniMidPos 1700
+              
+/**************************************
+		   	Bin Definitons
+**************************************/              
+
+#define grabBinPos 0
+#define releaseBinPos 2020
+              
+/**************************************
+			Camera Definitons
+**************************************/              
+
+#define redChannel 0
+#define greenChannel 1
+
+/**************************************
+	    Create Drive Definitons
+/**************************************/
+              
+#define createRightOffset 1
+//#define createLeftOffset 0.955
+#define createLeftOffset 0.952
+//#define createLeftOffset 0.99
+#define createDriveOffset 1
+#define createTurnOffset 0.92
+#define createDriveSpeed 250
+
+/**************************************
+    	Line Following Definitons
+**************************************/
+
+#define leftSensorPort 4
+#define rightSensorPort 5
+
+typedef enum {
+	SLOW,
+  	FAST,
+} ClawSpeed;
+
+void StartUp();
+void CreateDrive (float speed, float distance);
+void CreateDriveStraighter (float speed, float distance);
+void CreateDriveSlowDown (float startSpeed, float distance);
+void CreateDriveBack (float speed, float distance);
+void CreateWobble (int wobbleSpeed, float wobbleTime, int wobbles);
+void CreateWobbleBack (int wobbleSpeed, float wobbleTime, int wobbles);
+void CreateWobbleBackTill (int wobbleSpeed, float wobbleTime);
+void CreateDriveUntilTouch (float speed);
+void CreateSquareUp(float speed, float time);
+void CreateDriveUntil (float speed, float time);
+void CreateTurnLeft(float speed, int degrees);
+void CreateTurnRight(float speed, int degrees);
+//int createDriveUntilColor(float speed);
+void miniGrabberDown();
+void miniGrabberUp();
+void miniGrabberMid();
+void RaiseBin();
+void LowerBin();
+void clawDown();
+void clawUp();
+void clawCarry();
+void clawMid();
+void clawSquareUp();
+void clawOpen();
+void clawClose();
+void clawCloseSlow();
+void lineFollow(int blackValue, int whiteValue, float time, float speed);
+void lineFollowDistance(int blackValue, int whiteValue, float speed, float distance);
+int checkTribbles ();
+int FindColor();
+void SetTribbles(ClawSpeed speed);
+void LineTribbles();
+void CreateLineSquareUp(int speed, int blackValue, int whiteValue);
