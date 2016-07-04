6af02683e3098c4b5473fd65bd8f8995a9868e02
andywang02 <andywang02@gmail.com>
2016-07-04 10:52:54 -0700

    grasshopper 1.2

--- linkDrive.h
+++ linkDrive.h
@@ -7,13 +7,15 @@
 #define WHEEL_DIAMETER 2.2
 #define TICKS_PER_ROTATION 1100.0
 #define CM_TO_TICKS TICKS_PER_ROTATION / (PI*WHEEL_DIAMETER)
-#define LINK_LEFT_OFFSET 0.995
-#define LINK_RIGHT_OFFSET 0.8625 //Offset 0.865 at 20% - 30% battery
+#define LINK_LEFT_OFFSET 0.90
+#define LINK_RIGHT_OFFSET 0.91
 #define LINK_DRIVE_OFFSET 1
 
 void startUp ();
 void linkDrive (float distance, float speed);
 void linkDriveBack (float distance, float speed);
 void linkSquareUp (int time, float speed);
+void linkSquareUpBack(int time, float speed);
 void hop();
-void linkDriveToOpponentBase();
+void SpinReady();
+void spin(short direction, float rotations);
