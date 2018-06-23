0180d5e7aee64498a43191a57fb13dbda8f06d3c
Oscar Bjorkman <oscarrbjorkman@gmail.com>
2018-06-23 14:54:02 +0800

    Cleanup

--- DE Link #1/IGUSMain.c
+++ DE Link #1/IGUSMain.c
@@ -15,25 +15,19 @@ int main () {
       StartUp();
       blockFarEntrance();
     }
-  
-  //	blockFarEntrance(); 6/6
-  
-  	
-  
-  	ao();
+    ao();
     return 0;
 }
 
 void blockFarEntrance() {
-  
-  	CardboardMid();
+    CardboardMid();
     set_servo_position(2, 800);
     msleep(200);
-  	LinkDrive(2, 1600);
+    LinkDrive(2, 1600);
     set_servo_position(2, 1500);
     msleep(100);
     mav(LEFT_PORT, 1600*LINK_LEFT_OFFSET);
-  	msleep(200);
+    msleep(200);
     set_servo_position(2, 800);
     LinkDrive(3, 1600);
     set_servo_position(2, 1500);
@@ -45,23 +39,20 @@ void blockFarEntrance() {
     CardboardMid();
     mav(RIGHT_PORT, 1600*LINK_RIGHT_OFFSET);
     mav(LEFT_PORT, -1600*LINK_LEFT_OFFSET);
-  	msleep(200);
+    msleep(200);
   
     LinkDrive(8,1600);
     
-  	LinkThrowIgus();
-  	msleep(1750);
+    LinkThrowIgus();
+    msleep(1750);
     CardboardHi();
-  //  mav(RIGHT_PORT, 1600*LINK_RIGHT_OFFSET);
- // 	msleep(70);
-  
 }
 
 void blockMiddle() {
     CardboardMid();
     set_servo_position(2, 800);
     msleep(200);
-  	LinkDrive(3, 1600);
+    LinkDrive(3, 1600);
     set_servo_position(2, 1500);
     msleep(100);
     set_servo_position(2, 800);
@@ -73,40 +64,13 @@ void blockMiddle() {
     set_servo_position(2, 1500);
     LinkDrive(10, 1600);
     CardboardLow();
-  //  LinkDrive(9, 1600);
-  	/*
-    LinkDriveSlanted(870, 1200, 3500);
-    LinkSquareUp(1500, 2500);
-  	LinkDriveSlanted(870, 2000, 3500);
-    LinkSquareUp(1500, 2500);
-    */
-
-  	mav(LEFT_PORT, 1000*LINK_LEFT_OFFSET);
+  
+    mav(LEFT_PORT, 1000*LINK_LEFT_OFFSET);
     mav(RIGHT_PORT, -1000*LINK_LEFT_OFFSET);
-  	msleep(250);
+    msleep(250);
   
     LinkDrive(17, 1600);
-  
-  //  mav(LEFT_PORT, 1000*LINK_LEFT_OFFSET);
-  //  msleep(30);
-  
- // 	mav(RIGHT_PORT, 0);
- //   mav(LEFT_PORT, 0);
- //   msleep(150);
-    
     LinkThrowIgus();
   	
-  	msleep(2000); //Waits for one second
-    /*
-  	LinkSquareUp(1500, 1000);
-  	LinkThrowArm();
-  	LinkSquareUp(1500, 1500);
-  	*/
-  
-  	/*
-    LinkDrive(20, 1500);
-  	LinkTurnRightTime(1500, 500);
-  	LinkSquareUp(3000, 1500);
- 	LinkThrowArm();	
-    */
+    msleep(2000); //Waits for one second
 }
